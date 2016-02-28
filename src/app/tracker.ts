6d96d85baca1fdf9da7cd8bbb098ca4e8385a681
Jeremy Wilken <gnomation@gnomeontherun.com>
2016-02-27 21:18:04 -0600

    setup routing

--- src/app/tracker.ts
+++ src/app/tracker.ts
@@ -1,17 +1,19 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {StockInterface} from './services/stocks/stocks';
+import {Dashboard} from './components/dashboard/dashboard';
 import {Manage} from './components/manage/manage';
 
 @Component({
   selector: 'tracker-app',
   providers: [],
   templateUrl: 'app/tracker.html',
-  directives: [ROUTER_DIRECTIVES, Manage],
+  directives: [ROUTER_DIRECTIVES],
   pipes: []
 })
 @RouteConfig([
-
+  {path: '/', name: 'Dashboard', component: Dashboard},
+  {path: '/manage', name: 'Manage', component: Manage}
 ])
 export class TrackerApp {
 
